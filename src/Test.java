01e9aa746c3f7a3aa84bb019f3f42ceb7a392766
s-abeysekerat <s-abeysekerat@53S500036640.it.bsd405.org>
2017-10-24 16:24:54 -0700

    .

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Test {
 
@@ -5,5 +6,26 @@ public class Test {
 
 	}
 
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		System.out.println("How Many Potatoes do you want?");
+		int input = console.nextInt();
+		
+		if (input <= 0) {
+			System.out.println("NO POTATOES FOR YOU!!!");
+			System.out.println("OFF TO HELL FOR YOU!!!");
+			for (int i = 1; i <= Math.abs(input) + 1000;i++) {
+				System.out.println("POTATO HELL!");
+		}
+	} else if (input > 0) {
+		for (int i = 1; i <= input + 100;i++) {
+			System.out.println(i + " Potatoes");
+			}
+		}
+		
+		console.close();
+	}
+
 }
-system.out.printin(TestRepos)
+systemain(String[])m.out.printin(TestRepos)
--- src/file.java
+++ src/file.java
@@ -7,5 +7,4 @@
  *
  */
 public final class file {
-
 }
